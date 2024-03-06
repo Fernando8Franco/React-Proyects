e494189e0cc46400520a4c3b628cdde5d7a3c24d
Fernando8Franco <francoduranluisfernando@gmail.com>
2024-03-05 19:22:25 -0600

    Proyect started

---
+++ 03-learging-fetch-data/src/App.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react'
+
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/cute/says/${firstWord}?json=true`
+
+function App () {
+  const [fact, setFact] = useState()
+
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then(res => res.json())
+      .then(data => setFact(data.fact))
+  }, [])
+
+  return (
+    <main>
+      <h1>Cat App</h1>
+      {fact && <p>{fact}</p>}
+    </main>
+  )
+}
+
+export default App
