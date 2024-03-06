c47acb93cb0b8f782158f2d568d0f38f18c56eac
Fernando8Franco <francoduranluisfernando@gmail.com>
2024-03-05 18:10:10 -0600

    Second proyect finished

---
+++ 02-mouse-follower/src/App.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from 'react'
+
+function App () {
+  const [enable, setEnable] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  // Pointer Move
+  useEffect(() => {
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if (enable) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enable])
+
+  // Change body className
+  useEffect(() => {
+    document.body.classList.toggle('no-cursor', enable)
+
+    return () => {
+      document.body.classList.remove('no-cursor')
+    }
+  }, [enable])
+
+  return (
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: '#89f',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}
+      />
+      <button onClick={() => setEnable(!enable)}>
+        {enable ? 'Desactivar' : 'Activar'} seguir puntero
+      </button>
+    </main>
+  )
+}
+
+export default App
