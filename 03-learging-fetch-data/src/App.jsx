bca3bd17db7700d0762c8f82873386ab7067fec6
Fernando8Franco <francoduranluisfernando@gmail.com>
2024-03-06 19:27:03 -0600

    useEffect for fetch data & App.css added

--- 03-learging-fetch-data/src/App.jsx
+++ 03-learging-fetch-data/src/App.jsx
@@ -1,21 +1,43 @@
 import { useEffect, useState } from 'react'
+import './App.css'
 
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
-// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/cute/says/${firstWord}?json=true`
 
 function App () {
   const [fact, setFact] = useState()
+  const [imageURL, setImageURL] = useState()
 
+  // Get fact
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
       .then(res => res.json())
-      .then(data => setFact(data.fact))
+      .then(data => {
+        const { fact } = data
+        setFact(fact)
+      })
   }, [])
+
+  // Get image from random fact
+  useEffect(() => {
+    if (!fact) return
+
+    const firstTwoWords = fact.split(' ', 2).join(' ')
+
+    fetch(`https://cataas.com/cat/cute/says/${firstTwoWords}`)
+      .then(res => res)
+      .then(data => {
+        const { url } = data
+        setImageURL(url)
+      })
+  }, [fact])
 
   return (
     <main>
       <h1>Cat App</h1>
-      {fact && <p>{fact}</p>}
+      <section>
+        {fact && <p>{fact}</p>}
+        {imageURL && <img className='cat-image' src={imageURL} alt='Image generated from the first two words of the random fact' />}
+      </section>
     </main>
   )
 }
