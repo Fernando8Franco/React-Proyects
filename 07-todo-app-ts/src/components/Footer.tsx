fd378ffe64bb2bbe4cabb038173e0551425084fe
Fernando8Franco <francoduranluisfernando@gmail.com>
2024-04-24 12:01:19 -0600

    Todo app with typescript

---
+++ 07-todo-app-ts/src/components/Footer.tsx
@@ -0,0 +1,42 @@
+import { type FilterValue } from '../types'
+import { Filters } from './Filters'
+
+interface Props {
+  activeCount: number
+  completedCount: number
+  filterSelected: FilterValue
+  onClearCompleted: () => void
+  handleFilterChange: (filter: FilterValue) => void
+}
+
+export const Footer: React.FC<Props> = ({
+  activeCount,
+  completedCount,
+  filterSelected,
+  onClearCompleted,
+  handleFilterChange
+}) => {
+  return (
+    <footer className="footer">
+      <span className="todo-count">
+        <strong>{activeCount}</strong> Pending tasks
+      </span>
+
+      <Filters
+        filterSelected={filterSelected}
+        onFilterChange={handleFilterChange}
+      />
+
+      {
+        completedCount > 0 && (
+          <button
+            className='clear-completed'
+            onClick={onClearCompleted}
+          >
+            Clear completed task
+          </button>
+        )
+      }
+    </footer>
+  )
+}
